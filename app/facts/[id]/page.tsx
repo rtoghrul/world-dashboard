d3f832fa6126c4fe635607cf0e47c1dc3dd441ca
rtoghrul <eagleeye385@gmail.com>
2026-05-14 13:27:56 +0200

    Use news style facts page

--- app/facts/[id]/page.tsx
+++ app/facts/[id]/page.tsx
@@ -1,4 +1,4 @@
-import FactsCategoryClientV2 from '@/components/FactsCategoryClientV2'
+import FactsNewsStylePage from '@/components/FactsNewsStylePage'
 import { factCategories, getFactCategory } from '@/lib/facts'
 
 export function generateStaticParams() {
@@ -20,5 +20,5 @@ export default function DirectFactsCategoryPage({
   params: { id: string }
   searchParams?: { page?: string }
 }) {
-  return <FactsCategoryClientV2 id={params.id} page={searchParams?.page} />
+  return <FactsNewsStylePage id={params.id} page={searchParams?.page} />
 }
